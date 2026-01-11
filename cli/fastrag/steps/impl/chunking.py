0cbb77357af36788bbb5ea7993175bcf8688d85f
ZcR-01 <zihan1048@live.com>
2026-01-11 13:25:21 +0100

    feat: refactor get_tasks method to use inject for task instance creation

--- cli/fastrag/steps/impl/chunking.py
+++ cli/fastrag/steps/impl/chunking.py
@@ -6,6 +6,7 @@ from fastrag.config.config import Chunking
 from fastrag.events import Event
 from fastrag.steps.step import IStep
 from fastrag.steps.task import Task
+from fastrag.plugins import PluginRegistry, inject
 
 
 @dataclass
@@ -16,16 +17,15 @@ class ChunkingStep(IStep):
     step: list[Chunking]
 
     @override
-    async def get_tasks(self, cache: ICache) -> Dict[Task, AsyncGenerator[Event, None]]:
+    async def get_tasks(
+        self, cache: ICache
+    ) -> Dict[Task, List[AsyncGenerator[Event, None]]]:
         tasks = {}
         for s in self.step:
-            instance = PluginRegistry.get_instance(
-                System.CHUNKING, s.strategy, cache=cache, **s.params
-            )
+            instance = inject(Task, s.strategy, cache=cache, **s.params)
             entries = await cache.get_entries(
                 instance.filter
             )
-
             tasks[instance] = [instance.callback(uri, entry) for uri, entry in entries]
 
         return tasks
